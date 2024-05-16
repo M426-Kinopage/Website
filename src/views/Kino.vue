81aa3bf28c9bd7a3cb7cae3f56a212223ed1d970
Lars Stalder <lrsstalder@gmail.com>
2024-05-16 09:53:07 +0200

    Bilder hinzugefügt und Popups erstellt
    Ich habe die Popups erstellt und bilder hinzugefügt.

--- src/views/Kino.vue
+++ src/views/Kino.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>
-      <h1>Kino Page</h1>
-      <router-link to="/">Home</router-link>
-      <!-- Weitere Inhalte der Partnerseite -->
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Kino',
-  }
-  </script>
+  <div>
+    <h1>Kino Page</h1>
+    <!-- Weitere Inhalte der Partnerseite -->
+    <button @click="showPopup = true">Zeige Popup</button>
+    <Wokami v-if="showPopup" @close="showPopup = false" />
+  </div>
+</template>
+
+<script>
+import Wokami from "./Wokami.vue";
+
+export default {
+  name: "Kino",
+  components: {
+    Wokami,
+  },
+  data() {
+    return {
+      showPopup: false,
+    };
+  },
+};
+</script>
+
+<style>
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
